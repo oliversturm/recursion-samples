b18870b5a39f870c90a9380bdfdd7c0bedca02cb
Oli Sturm <oliver+commitsigning@oliversturm.com>
2023-02-08 16:34:06 +0000

    adding ping/pong samples

---
+++ CSPingPong/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program {
+  static int Ping(int c, int v) {
+    return c == 0 ? v : Pong(c - 1, v + c);
+  }
+
+  static int Pong(int c, int v) {
+    return c == 0 ? v : Ping(c - 1, v + c);
+  }
+
+  public static void Main() {
+    Console.WriteLine($"Ping/pong result: {Ping(300000, 0)}");
+  }
+}
---
+++ FSPingPong/Program.fs
@@ -0,0 +1,19 @@
+﻿let rec ping c v =
+    match c with
+    | 0 -> v
+    | _ -> pong (c - 1) (v + c)
+
+and pong c v =
+    match c with
+    | 0 -> v
+    | _ -> ping (c - 1) (v + c)
+
+printfn "Ping/pong result: %d" (ping 300000 0)
+
+// let rec timesTwo x = (plus5 x) * 2
+
+// and plus5 x = x + 5
+
+
+
+// printfn "Thing: %d" (timesTwo 10)
